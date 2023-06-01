eb6d443ab6813ab30acddd49bfe0098d7cd93ba6
Jiaxin sugar <59117479+path-yu@users.noreply.github.com>
2023-06-01 16:28:40 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ async function handler(req: Request): Promise<Response> {
       exchange_rate: rate,
     });
     return new Response(body, {
-      status: 404,
+      status: 200,
       headers: {
         "content-type": "application/json; charset=utf-8",
       },
