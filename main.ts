938d41fe2328e45cf40fed5d7e1a46fef22e7fba
Jiaxin sugar <59117479+path-yu@users.noreply.github.com>
2023-06-01 16:48:42 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,33 @@
-import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
-import { Request } from "https://deno.land/x/request@1.3.2/mod.ts";
 import * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
-// To listen on port 4242.
-serve(handler, { port: 4242 });
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
-async function handler(req: Request): Promise<Response> {
-  const url = new URL(req.url);
-  const searchParams = url.searchParams;
-  const start = searchParams.get("start") || "USD";
-  try {
-    const textResponse = await fetch(
-      `https://www.google.com/finance/quote/${start}-CNY`,
-    );
-    const textData = await textResponse.text();
-    const $ = cheerio.load(textData);
-    const rate = $(".kf1m0 .fxKbKc").text();
-    const body = JSON.stringify({
-      target_currency: start,
-      exchange_rate: rate,
-    });
-    return new Response(body, {
-      status: 200,
-      headers: {
-        "content-type": "application/json; charset=utf-8",
-      },
-    });
-  } catch (err) {
-    throw err;
-  }
-}
+const router = new Router();
+router
+  .get("/", async (context) => {
+    const url = new URL(context.request.url);
+    const searchParams = url.searchParams;
+    const start = searchParams.get("start") || "USD";
+    try {
+      const textResponse = await fetch(
+        `https://www.google.com/finance/quote/${start}-CNY`,
+      );
+      const textData = await textResponse.text();
+      const $ = cheerio.load(textData);
+      const rate = $(".kf1m0 .fxKbKc").text();
+      // const body = JSON.stringify();
+      context.response.type = "application/json";
+      context.response.body = {
+        target_currency: start,
+        exchange_rate: rate,
+      };
+    } catch (err) {
+      throw err;
+    }
+  });
+const app = new Application();
+app.use(oakCors()); // Enable CORS for All Routes
+app.use(router.routes());
+
+console.info("CORS-enabled web server listening on port 8000");
+await app.listen({ port: 8000 });
